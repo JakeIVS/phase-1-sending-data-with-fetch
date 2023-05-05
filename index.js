2481ac7335ad4b818e4c496b9961453c819b2922
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-05 12:32:18 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+        'name': name,
+        'email': email
+    }
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify(userData),
+    })
+    .then((r)=>r.json())
+    .then((data)=> {
+        const p = document.createElement('p')
+        const body = document.querySelector('body')
+        p.innerHTML = data.id;
+        body.appendChild(p);
+    })
+    .catch((error)=>{
+            const p = document.createElement('p')
+            const body = document.querySelector('body')
+            p.innerHTML = error.message;
+            body.appendChild(p);
+    });
+}
